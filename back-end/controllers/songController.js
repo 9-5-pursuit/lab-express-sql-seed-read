41d3ba178060d9858cbf3cc26bfdfb1cdc5a04eb
davep80 <davidpaq1@gmail.com>
2023-07-17 13:46:11 -0400

    post, get, delete

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const { getAllSongs } = require('../queries/songs');
+const { getOneSong } = require('../queries/songs');
+const { addRow } = require('../queries/songs');
+const { deleteRow } = require('../queries/songs');
+const { updateRow } = require('../queries/songs');
 //const bookmark = require()
 const router = express.Router();
 
 router.get('/', async(req, res) => {
     const allSongs = await getAllSongs();
     if (allSongs[0]) res.json(allSongs);
+    else res.status(500).json({err: 'pg error'})
+})
+
+router.get('/:id', async(req, res) => {
+    const id = req.params.id
+    const allSongs = await getOneSong(id);
+    if (allSongs[0]) res.json(allSongs);
+    else res.status(500).json({err: 'pg error'})
+})
+
+router.post('/', async(req, res) => {
+    const newRow = await addRow(Object.values(req.body));
+    if (newRow[0]) res.json(newRow);
+    else res.status(500).json({err: 'pg error'})
+})
+
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id
+    const status = await deleteRow(id);
+    if (status[0]) res.json(status[0]);
+    else res.status(500).json({err: 'pg error'})
+})
+
+router.put('/:id', async(req, res) => {
+    const id = req.params.id
+    const updated = await updateRow(id, Object.values(req.body));
+    if (updated[0]) res.json(updated[0]);
     else res.status(500).json({err: 'pg error'})
 })
 
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -6,7 +6,40 @@ const getAllSongs = async() => {
         return allSongs;
     } catch (e) { return e }
 };
+const getOneSong = async(args) => {
+    try {
+        const Song = await db.any(`SELECT * FROM songs where id = ${args}`);
+        return Song;
+    } catch (e) { return e }
+};
+
+const addRow = async(args) => {
+    try {
+        const Row = await db.any(`INSERT INTO songs (name, artist, album, time, is_favorite) 
+        VALUES ('${args[0]}','${args[1]}','${args[2]}','${args[3]}',${args[4]}) RETURNING *`);
+        return Row;
+    } catch (e) { return e }
+};
+
+const deleteRow = async(args) => {
+    try {
+        const Del = await db.any(`DELETE FROM songs where id = ${args} RETURNING *`);
+        return Del;
+    } catch (e) { return e }
+};
+
+const updateRow = async(args, argsb) => {
+    try {
+        const Updated = await db.any(`UPDATE songs SET name = '${args[0]}', artist = '${args[1]}', album = '${args[2]}'
+        , time = '${args[3]}', is_favorite = ${args[4]} WHERE id = ${args} RETURNING *`);
+        return Updated;
+    } catch (e) { return e }
+};
 
 module.exports = {
     getAllSongs,
+    getOneSong,
+    addRow,
+    deleteRow,
+    updateRow,
 }
