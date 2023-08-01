7640f4a5ac87251bbc7a183dd52ba38e386cfa37
Mussie <mussierussom@pursuit.org>
2023-08-01 09:51:01 -0400

    lab Tuner

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,11 +3,11 @@
 INSERT INTO 
     songs(name, artist, album, time, is_favorite) 
 VALUES 
-('I am every woman', 'Chaka Khan', 'Chaka', 4:20, true), 
-('love after love', 'Cher','Believe', 4:19, true),
-('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', 5:55, true),
-('Boogie Wonderland', 'Earth, Wind & Fire', 'I Am',4:48, false),
-('What Does not Kill You', 'Kelly Clarkson', 'Breakaway', 3:39, false),
+('I am every woman', 'Chaka Khan', 'Chaka', 4.20, true), 
+('love after love', 'Cher','Believe', 4.19, true),
+('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', 5.55, true),
+('Boogie Wonderland', 'Earth, Wind & Fire', 'I Am',4.48, false),
+('What Does not Kill You', 'Kelly Clarkson', 'Breakaway', 3.39, false),
     ('Fame', 'David Bowie', 'Young Americans', '4:12', true ),
     ('Once in a Lifetime', 'Talking Heads', 'Remain in Light', '4:19', true ),
     ('The Great Curve', 'Talking Heads', 'Sand in the Vaseline', '5:39', true ),
